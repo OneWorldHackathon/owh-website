2938fb5832191dba56e098d1d7315dcac73f6df4
jcsmillie <jonnysmillie@jonnysmillie.co.uk>
2019-01-06 20:49:35 +0000

    sort logo on mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,12 +27,12 @@ class Header extends React.Component {
         <div className="container">
           <div className="logo">
             <Link to="/">
-              <img alt="Figurit  Homepage" src={logo} />
+              <img alt="OWH Homepage" src={logo} />
             </Link>
           </div>
           <div className="logo-mobile">
             <Link to="/">
-              <img alt="Figurit Homepage" src={logoMobile} />
+              <img alt="OWH Homepage" src={logo} />
             </Link>
           </div>
           <MenuMobile active={this.state.menuActive} />
